b767f838f02b6e3151d3206599b805c73b24a452
Yisheng Cai <cai.eason@gmail.com>
2017-02-03 23:22:28 -0500

    feature: add eslint rules for code styling

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,5 +4,11 @@ module.exports = {
         "react",
         "jsx-a11y",
         "import"
-    ]
+    ],
+    rules: {
+      'react/jsx-filename-extension': [2, { extensions: ['.js'] }],
+      'import/prefer-default-export': 0,
+      'no-unused-vars': [2, { args: 'none', "varsIgnorePattern": "^_$", }],
+      'react/prop-types': [0],
+    },
 };
