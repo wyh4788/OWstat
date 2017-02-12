5b5534542d9b1fc32407f7d196c8ac6a4fd57dcb
wyh4788 <yinghuanwang521@gmail.com>
2017-02-12 16:50:01 -0500

    Revert "Add feature: add multiply button"
    This reverts commit a1fc9f5948fe388effc291044eec8aca60f8c237.

--- app/containers/stats/stats.js
+++ app/containers/stats/stats.js
@@ -3,12 +3,24 @@ import { connect } from 'react-redux';
 import { fetchUserData } from '../../actions/fetch-user-data/fetch-user-data';
 import Stats from '../../components/counter';
 
+<<<<<<< HEAD
+=======
+const addNumAction = () => ({
+  type: 'ADD_ONE',
+  increment: 1,
+});
+
+>>>>>>> parent of a1fc9f5... Add feature: add multiply button
 const mapStateToProps = state => ({
   userStats: state.userStats,
 });
 
 const mapDispatchToProps = dispatch => ({
+<<<<<<< HEAD
   fetchUserData: userId => dispatch(fetchUserData(userId)),
+=======
+  incrementNum: () => dispatch(addNumAction()),
+>>>>>>> parent of a1fc9f5... Add feature: add multiply button
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(Stats);
--- app/reducers/stats-data/index.js
+++ app/reducers/stats-data/index.js
@@ -1,13 +1,24 @@
 // @flow
 const defaultStats = {};
 
+<<<<<<< HEAD
 export const statsReducer = (state = defaultStats, action) => {
+=======
+// import actionNames from '../../constants/action-names';
+
+const defaultStatsData = {
+  counter: 0,
+};
+
+export const statsDataReducer = (state = defaultStatsData, action) => {
+>>>>>>> parent of a1fc9f5... Add feature: add multiply button
   switch (action.type) {
     case 'FETCH_USER_DATA':
       return {
         ...state,
         userStats: action.payload,
       };
+<<<<<<< HEAD
 
     case 'USER_DATA_SUCCESS':
       return {
@@ -15,6 +26,8 @@ export const statsReducer = (state = defaultStats, action) => {
         userStats: action.userStats,
       };
 
+=======
+>>>>>>> parent of a1fc9f5... Add feature: add multiply button
     default:
       return state;
   }
