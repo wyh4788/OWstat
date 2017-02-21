93a6fd9b23b91fdc9dea4e9a99afacd05c66b14c
Yisheng Cai <cai.eason@gmail.com>
2017-02-20 19:23:10 -0500

    feature: add tabs

--- app/containers/router.js
+++ app/containers/router.js
@@ -3,14 +3,30 @@
 import React from 'react';
 import { Scene, Router } from 'react-native-router-flux';
 import Stats from './stats/stats';
+import Settings from './settings/settings';
 
 export const NavigationRouter = () => (
   <Router>
     <Scene
-      key="user-stats"
-      title="OW Stats"
-      component={Stats}
-    />
+      key="tabs"
+      tabs
+    >
+      <Scene
+        key="user-stats"
+        title="My Stats"
+        component={Stats}
+      />
+      <Scene
+        key="hero-stats"
+        title="Hero Stats"
+        component={Stats}
+      />
+      <Scene
+        key="setting"
+        title="Settings"
+        component={Settings}
+      />
+    </Scene>
   </Router>
 );
 
